672637388800d912aeb37935994f31f6b91f6fc3
Tahsin007 <77976636+Tahsin007@users.noreply.github.com>
2023-08-14 03:23:01 +0600

    Create TV.java

---
+++ Facade/TV.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Design_Pattern.Facade;
+
+/**
+ *
+ * @author TANIM
+ */
+public class TV {
+    public void on(){
+        System.out.println("TV is on");
+    }
+    public void off(){
+        System.out.println("TV is off");
+    }
+    public void setChannel(){
+        System.out.println("Channel is set");
+    }
+    
+}
